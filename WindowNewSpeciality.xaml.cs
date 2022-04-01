98eb5518bec5155f615726562432c3708ad5cd9f
daniilka5277 <48345753+daniilka5277@users.noreply.github.com>
2022-04-01 18:50:51 +0300

    NewWindow

---
+++ WindowNewSpeciality.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Variant_11.NewWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowNewSpeciality.xaml
+    /// </summary>
+    public partial class WindowNewSpeciality : Window
+    {
+        public WindowNewSpeciality()
+        {
+            InitializeComponent();
+        }
+
+        private void BtSave_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
